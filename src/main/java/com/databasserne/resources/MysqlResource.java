3b6313f4c483d878c5114843a32d940b7d921173
Martin Karlsen <1martinkarlsen@gmail.com>
2017-05-22 10:08:31 +0200

    feat(Rest): Get cities from book title

--- src/main/java/com/databasserne/resources/MysqlResource.java
+++ src/main/java/com/databasserne/resources/MysqlResource.java
@@ -6,7 +6,6 @@
 package com.databasserne.resources;
 
 import com.databasserne.repo.implementation.MysqlBooksRepo;
-import com.databasserne.repo.implementation.StubBooksRepo;
 import com.databasserne.repo.interfaces.IBooksRepo;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -14,12 +13,9 @@ import java.sql.SQLException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -43,11 +39,22 @@ public class MysqlResource {
         //booksRepo = new StubBooksRepo();
         booksRepo = new MysqlBooksRepo();
     }
+    
+    public MysqlResource(IBooksRepo repo) {
+        booksRepo = repo;
+    }
 
     @GET
     @Path("city/{city}")
     @Produces("application/json")
     public Response searchCity(@PathParam("city") String city) {
         return Response.status(Response.Status.OK).entity(gson.toJson(booksRepo.getBooksAndAuthorFromCity(city))).build();
+    }
+    
+    @GET
+    @Path("book/{bookTitle}")
+    @Produces("application/json")
+    public Response searchCitiesFromBookTitle(@PathParam("bookTitle") String bookTitle) {
+        return Response.status(Response.Status.OK).entity(gson.toJson(booksRepo.getCitiesFromBookTitle(bookTitle))).build();
     }
 }
